1c4197812c0676fb61a28fc19c4c86b1507edaa7
nbela <nbelaib88@gmail.com>
2025-11-04 15:42:29 +0200

    Page d'accueil

--- src/classes/action/DefaultAction.php
+++ src/classes/action/DefaultAction.php
@@ -1,19 +1,43 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace iutnc\netvod\action;
 
-class DefaultAction extends Action {
-
+class DefaultAction extends Action
+{
+    /**
+     * Méthode GET
+     * -> Affiche page d'accueil : différente si utilisateur connecté ou non.
+     */
     public function executeGet(): string
     {
-        // TODO: Implement executeGet() method.
+        // Si l’utilisateur est connecté
+        if (isset($_SESSION['user'])) {
+            $user = $_SESSION['user'];
+            $firstname = htmlspecialchars($user['firstname'] ?? '');
+
+            return <<<HTML
+                <h1>Bienvenue sur NetVOD, {$firstname} !</h1>
+                <p>Accédez à votre <a href="?action=catalog">catalogue de séries</a> ou retrouvez vos 
+                <a href="?action=view-favorites">séries préférées</a>.</p>
+            HTML;
+        }
+
+        // Sinon, affichage pour visiteur
+        return <<<HTML
+            <h1>Bienvenue sur NetVOD </h1>
+            <p>Explorez notre catalogue de séries en vous <a href="?action=login">connectant</a>
+            ou en <a href="?action=register">créant un compte</a> gratuitement.</p>
+        HTML;
     }
 
+    /**
+     * Méthode POST
+     * -> Dans ce cas précis, rien à traiter en POST.
+     */
     public function executePost(): string
     {
-        // TODO: Implement executePost() method.
+        return $this->executeGet();
     }
-
 }
