ee292c37fe07f770cc8d347a9a4134f4b1412de2
Tristan <tristadam10@gmail.com>
2025-11-05 09:06:40 +0100

    Modification script bdd

--- sql/netvod.sql
+++ sql/netvod.sql
@@ -35,16 +35,23 @@ CREATE TABLE `series` (
     `img` VARCHAR(100) DEFAULT NULL,
     `year` INT(4) DEFAULT NULL,
     `date_added` DATE NOT NULL,
-    `genre_id` INT(11) DEFAULT NULL,
     PRIMARY KEY (`series_id`),
     FOREIGN KEY (`genre_id`) REFERENCES `genre` (`genre_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
+-- Table d'association `genre_series`
+-- --------------------------------------------------------
+CREATE TABLE `genre_series` (
+    `genre_id` INT(11) NOT NULL,
+    `series_id` INT(11) NOT NULL,
+    PRIMARY KEY (`genre_id`, `series_id`),
+    FOREIGN KEY (`genre_id`) REFERENCES `genre` (`genre_id`),
+    FOREIGN KEY (`series_id`) REFERENCES `series` (`series_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
 -- Table `episode`
--- (Note: L'association Episode2Series dans le schéma UML suggère une relation N-N entre Episode et Series,
--- mais l'entité Episode a généralement une FK vers Series. J'ai respecté l'entité `episode` du schéma,
--- mais en pratique `Episode2Series` est redondante si `episode` a déjà `series_id`. Je crée `episode` sans FK vers `series` pour l'instant et j'utilise `Episode2Series` pour la liaison N-N stricte.)
 -- --------------------------------------------------------
 CREATE TABLE `episode` (
     `episode_id` INT(11) NOT NULL AUTO_INCREMENT,
